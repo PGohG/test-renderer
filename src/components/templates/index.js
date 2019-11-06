4c7aef5a75eca58a3a376e8103e1cc13a8cca999
PGohG <gochbi@gmail.com>
2019-11-06 19:16:03 +0800

    Test1

--- src/components/templates/index.js
+++ src/components/templates/index.js
@@ -8,11 +8,13 @@ import DefaultTemplate from "./default";
 import CustomTemplate from "./customTemplate";
 import GovTechDemoCert from "./govtechDemoCert";
 import ITE2020FulCert from "./ITE-2020-FUL-CERT-FULL1";
+import ITE2020FulCertNoTS from "./ITE-2020-FUL-CERT-FULL1-NO-TSCPT";
 
 export default {
   default: DefaultTemplate,
   CUSTOM_TEMPLATE: CustomTemplate,
   GOVTECH_DEMO: GovTechDemoCert,
   ITE_2020_FUL_CERT_FULL1: ITE2020FulCert,
+  ITE_2020_FUL_CERT_FULL1_NO_TSCPT: ITE2020FulCertNoTS,
   NULL: []
 };
