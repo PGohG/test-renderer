3446245c7cffc6db2292e8ee6165d9e84186fd80
PGohG <gochbi@gmail.com>
2020-02-05 18:08:38 +0800

    TestV2.1

--- src/components/templates/common/recipient.js
+++ src/components/templates/common/recipient.js
@@ -29,13 +29,22 @@ const Recipient = ({ doc }) => (
 	} </div>
 
    <div> {doc.additionalData.transcriptData.addressLine2
-	? <div className="row">
-		  <div className="col-2">  </div>
-   		  <div className="col-9"> &nbsp; &nbsp; {doc.additionalData.transcriptData.addressLine2} </div>
-	   </div>
+	? <div> {!doc.additionalData.transcriptData.addressLine3 && !doc.additionalData.transcriptData.addressLine4
+		? <div> {doc.additionalData.transcriptData.graduationField.includes("DATE OF GRADUATION")
+			? <div className="row">
+			  	<div className="col-2">  </div>
+				<div className="col-5"> &nbsp; &nbsp; {doc.additionalData.transcriptData.addressLine2} </div>
+				<div className="col-4"> {doc.additionalData.transcriptData.graduationField} : &nbsp; {doc.additionalData.transcriptData.graduationDates} </div>
+			  </div>
+			: <div> <div className="row">
+			  	<div className="col-2">  </div>
+			  	<div className="col-9"> &nbsp; &nbsp; {doc.additionalData.transcriptData.addressLine2} </div>
+			  </div>
+		   } </div>
+		: null
+		} </div>
 	: null
 	} </div>
-
 
    <div> {doc.additionalData.transcriptData.addressLine3
 	? <div> {doc.additionalData.transcriptData.addressLine4
@@ -89,6 +98,8 @@ const Recipient = ({ doc }) => (
 		: null
 		} </div>
 	} </div>
+
+	<div className="row"> <br /> </div>
 
     <div className="row">
       <div className="col-3"> ACADEMIC CAREER </div>
